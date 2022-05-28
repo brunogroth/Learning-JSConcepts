705d082fa3099fffa47d0096bb3731ff79d9fe44
Bruno Groth <brunom.groth@gmail.com>
2022-05-27 23:51:54 -0300

    Exemplo de trabalho de criação de estruturas HTML em JS usando consulta em arquivos JSON

--- ProjetoPrimeitoCommit/script3.js
+++
@@ -1,9 +0,0 @@
-function criarSonar() {
-      
-      window.open("https://blog.myscrumhalf.com/sonar-apoiando-a-qualidade-do-desenvolvimento-de-software/","_blank");
-      
-}
-    var botoes = document.querySelectorAll('button.sonar');
-    for(var i = 0; i < botoes.length ; i++) {
-      botoes[i].addEventListener('click', criarSonar);
-}
--- ProjetoPrimeitoCommit/script4.js
+++
@@ -1,7 +0,0 @@
-function criarJenkins() {
-       window.open("https://www.gocache.com.br/dicas/o-que-e-jenkins-para-iniciantes/")
-}
-    var botoes = document.querySelectorAll('button.jenkins');
-    for(var i = 0; i < botoes.length ; i++) {
-      botoes[i].addEventListener('click', criarJenkins);
-}
